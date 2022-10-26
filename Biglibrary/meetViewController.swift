fe586dbe9226724fecf1e19bc933eab6220da2ac
saturn777 <aleksandr-luchinin@bk.ru>
2022-10-25 18:03:20 -0700

    Создаю кнопку входу и картинку

--- Biglibrary/meetViewController.swift
+++ Biglibrary/meetViewController.swift
@@ -7,18 +7,43 @@
 
 import UIKit
 
-class meetViewController: UIViewController {
+final class meetViewController: UIViewController {
+
+    
+   var myBotton = UIButton()
+    var myImageView = UIImageView()
+    let libraryImage = UIImage(named: "library")
 
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+//         Кнопка вход
+        myBotton = UIButton(type: .roundedRect)
+        myBotton.frame = CGRect(x: 110, y: 200, width: 100, height: 44)
+        myBotton.backgroundColor = .blue
+        myBotton.setTitle("Вход", for: .normal)
+        myBotton.setTitle("Ok", for: .highlighted)
+        myBotton.layer.cornerRadius = 8
+        myBotton.addTarget(self, action: #selector(buttonIsPressed), for: .touchDown)
+        myBotton.addTarget(self, action: #selector(buttonIsTapped), for: .touchUpInside)
+        self.view.addSubview(myBotton)
         
+//        Картинка Библиотека
+        myImageView = UIImageView(frame: CGRect(x: 78, y: 10, width: 150, height: 150))
+        myImageView.clipsToBounds = true
+        myImageView.image = libraryImage
+        view.addSubview(myImageView)
         
         
     }
-    
-
+//    Создаю 2 метода, когда кнопка нажата и когда кнопку уже нажали
+    @objc func buttonIsPressed(sender: UIButton) {
+        
+    }
    
+    @objc func buttonIsTapped(sender: UIButton) {
+        
+    }
 }
